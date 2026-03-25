0c74e0461396465e5ef479c011f3124a0fc507c4
MADxGUNMAN <ansarisoyab908@gmail.com>
2026-03-25 17:03:44 +0530

    feat: Add sitemap and configure Next.js headers for sitemap.xml and Google site verification.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/sitemap.xml',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/xml',
+          },
+        ],
+      },
+      {
+        source: '/googled5c4c1dd6c92ddf6.html',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/html',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
